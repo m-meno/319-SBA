74ad4a7991999b1b5dc0ef8c7c6300542dfa118a
m-meno <mackenzie.meno@gmail.com>
2025-04-28 13:46:13 -0600

    All four basic request routes

---
+++ routes/requestRoutes.mjs
@@ -0,0 +1,37 @@
+import express from 'express';
+import Request from '../models/requestSchema.mjs';
+
+const router = express.Router();
+
+//Create
+router.post('/', async (req, res) => {
+    const newRequest = await Request.create(req.body);
+    res.json(newRequest);
+});
+
+//Read
+router.get('/', async (req, res) => {
+    const allRequests = await Request.find({});
+    res.json(allRequests);
+});
+
+//Update
+router.put('/:id', async (req, res) => {
+    const editRequest = await Offer.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!editRequest) {
+        res.status(400).json({ msg: `Offer not found.` })
+    };
+    res.json(editRequest);
+});
+
+//Delete 
+router.delete('/:id', async (req, res) => {
+    const deleteRequest = await Offer.findByIdAndDelete(req.params.id);
+    if (!deleteRequest) {
+        res.status(400).json('Offer not found.')
+    };
+});
+
+
+
+export default router;
